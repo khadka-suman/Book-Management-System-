255829d69572458a1bba34af885ccb6a505d4575
Suman Khadka <47713190+khadka-suman@users.noreply.github.com>
2022-06-24 21:35:55 +0545

    message

---
+++ Controllers/BooksContoller.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Book.Models;
+using Book.Areas.Identity;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Book.Areas.Identity.Data;
+
+
+namespace Book.Controllers
+{
+    public class BooksContoller : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public BooksContoller(ApplicationDbContext db)
+            
+
+       
+        {
+          
+        
+                   _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Books> objBooksList = _db.Books.ToList();
+            return View(objBooksList);
+
+        }
+
+        //GET
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create(Books obj)
+        {
+            if(ModelState.IsValid)
+            {
+
+            
+            _db.Books.Add(obj);
+            _db.SaveChanges();
+            return View ("Index");
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Edit(int? BookId)
+        {
+            if (BookId == null || BookId == 0)
+            {
+                return NotFound();
+
+            }
+            var BooksFromDb = _db.Books.Find(BookId);
+            if (BooksFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(BooksFromDb);
+
+        }
+        //GET Details
+       public async Task<IActionResult> Details(int? BookId)
+        {
+            if (BookId == null || BookId == 0)
+            {
+                return NotFound();
+            }
+            var Books = await _db.Books
+                .FirstOrDefaultAsync(u => u.BookId == BookId);
+            if (Books == null)
+            {
+                return NotFound();
+            }
+            return View(Books);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Books obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Books.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Books Updates Successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+
+
+        //GET
+        public IActionResult Delete(int? BookId)
+        {
+            if (BookId == null || BookId == 0)
+            {
+                return NotFound();
+            }
+            var BooksFromDb = _db.Users.Find(BookId);
+            if (BooksFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(BooksFromDb);
+        }
+
+
+        //POST  
+        [HttpPost]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? BookId)
+        {
+            var obj = _db.Books.Find(BookId);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            {
+                _db.Books.Remove(obj);
+                _db.SaveChanges();
+                TempData["sucess"] = "Book deleted Sucessfully";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
